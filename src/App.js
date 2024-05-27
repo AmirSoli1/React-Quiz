bace828c843d2daea7941e4a9e709e0e784a4cf9
Amir Solimany <amirsoli@post.bgu.ac.il>
2024-05-27 15:14:38 +0300

    add json server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  status: "loading", // loading, error, ready, active, finished
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_QUESTIONS":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "ERROR":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Unhandled action type: " + action.type);
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  console.log(state);
+
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:3001/questions");
+        const data = await response.json();
+        console.log(data);
+
+        dispatch({ type: "SET_QUESTIONS", payload: data });
+      } catch (error) {
+        dispatch({ type: "ERROR" });
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div>
+    <div className="app">
       <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question 1</p>
+      </Main>
     </div>
   );
 }
